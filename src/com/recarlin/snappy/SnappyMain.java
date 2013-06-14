bb8b14243830b06415739a2e360b440f19b44193
recarlin <recarlin@live.com>
2013-06-13 22:38:14 -0400

    Having some issues with creating a file.

--- src/com/recarlin/snappy/SnappyMain.java
+++ src/com/recarlin/snappy/SnappyMain.java
@@ -1,18 +1,35 @@
+/*
+ * project		Snappy
+ * package		com.recarlin.snappy
+ * author		Russell Carlin
+ * date			Jun 13, 2013
+ */
 package com.recarlin.snappy;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SnappyMain extends Activity {
-
-	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-	private Uri fileUri;
+	
+	private String photo;
+	private File image;
+	private static String FILE_TYPE;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +38,21 @@ public class SnappyMain extends Activity {
 		
 		Button newPic = (Button) findViewById(R.id.newPic);
 		newPic.setOnClickListener(new View.OnClickListener() {
+			@SuppressLint("SimpleDateFormat")
 			@Override
 			public void onClick(View v) {
+				File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Snappy");
+				String timeStamp = new SimpleDateFormat("MM-dd-yyyy_HH:mm:ss").format(new Date());
+				String imageFileName =  "pic_" + timeStamp;
+				try {
+					image = File.createTempFile(imageFileName, FILE_TYPE, storageDir);
+					photo = image.getAbsolutePath();
+				} catch (IOException e) {
+					Log.e("FILE CREATION", "There was an issue creating the file: " + imageFileName + "  " + storageDir);
+				}
 				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-			    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-			    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
-
-			    // start the image capture Intent
-			    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-			}
-		});
-		Button oldPic = (Button) findViewById(R.id.oldPic);
-		oldPic.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				
+				intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(image));
+				startActivityForResult(intent, 100);
 			}
 		});
 	}
@@ -48,4 +64,16 @@ public class SnappyMain extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Bitmap theImage = (Bitmap) data.getExtras().get("data");
+		ImageView iView = ((ImageView) findViewById(R.id.imageView));
+		iView.setImageBitmap(theImage);
+		
+		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+	    File f = new File(photo);
+	    Uri contentUri = Uri.fromFile(f);
+	    mediaScanIntent.setData(contentUri);
+	    this.sendBroadcast(mediaScanIntent);
+	}
 }
